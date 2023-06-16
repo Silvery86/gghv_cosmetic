1ea00a9928ad6c89d4ba725a8f7f0e0e142b597a
Nguyen Giang <giang8692@gmail.com>
2023-06-16 07:23:06 +0700

    add function to connect to database

---
+++ mvc/core/Database.php
@@ -0,0 +1,38 @@
+<?php 
+class Database{
+    public $conn = NULL; 
+	public $pdo_stm = NULL;
+   
+    protected $servername = "localhost";
+    protected $username = "root";
+    protected $password = "mysql";
+    protected $dbname = "gghv_cosmetic";
+
+    function __construct(){
+        try
+		{
+			$this->conn = new PDO("mysql:host=".$this -> servername.";dbname=".$this->dbname."","".$this->username."","".$this->password."");
+			$this->conn->exec("SET NAMES UTF8");//Thiết lập làm việc với unicode
+		}
+		catch(PDOException $ex)
+		{
+			echo "<h3>" . $ex->getMessage() . "</h3>";
+			die("<h3> LỖI KẾT NỐI CSDL </h3>");
+		}
+    }
+    function ProcessSQL($sql, $data=NULL)
+        {
+            $this->pdo_stm = $this->conn->prepare($sql);
+            $ketqua=false;
+            if($data!=NULL)
+                $ketqua = $this->pdo_stm->execute($data);
+            else
+                $ketqua = $this->pdo_stm->execute();
+            return $ketqua;
+        }
+
+   
+
+    
+}
+?>
