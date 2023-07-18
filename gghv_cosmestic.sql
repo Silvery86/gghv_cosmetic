6a9a6a068333c08ce3bc8d0ca9c8b31b4e4ef40c
Nguyen Giang <giang8692@gmail.com>
2023-07-18 22:46:21 +0700

    update database

--- gghv_cosmestic.sql
+++ gghv_cosmestic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: localhost
--- Thời gian đã tạo: Th7 18, 2023 lúc 02:43 PM
+-- Thời gian đã tạo: Th7 18, 2023 lúc 03:45 PM
 -- Phiên bản máy phục vụ: 8.0.31
 -- Phiên bản PHP: 7.4.33
 
@@ -89,18 +89,13 @@ CREATE TABLE `orders` (
   `id` int NOT NULL,
   `user_id` int NOT NULL,
   `order_address` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `customer_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `customer_phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `note` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `order_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `status` int DEFAULT NULL,
   `total_money` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Đang đổ dữ liệu cho bảng `orders`
---
-
-INSERT INTO `orders` (`id`, `user_id`, `order_address`, `note`, `order_date`, `status`, `total_money`) VALUES
-(1, 1, 'Nhà A5 Đền Lừ 2-Hà Nội-VietNam', 'asdasd', NULL, NULL, 264000);
 
 -- --------------------------------------------------------
 
@@ -113,8 +108,7 @@ CREATE TABLE `order_details` (
   `order_id` int DEFAULT NULL,
   `product_id` int DEFAULT NULL,
   `price` int DEFAULT NULL,
-  `num` int DEFAULT NULL,
-  `total_money` int DEFAULT NULL
+  `num` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -144,7 +138,8 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `category_id`, `brand_id`, `title`, `price`, `stock`, `discount`, `thumbnail`, `description`, `long_description`, `created_at`, `updated_at`, `deleted`) VALUES
-(2, 1, 3, 'Nước Tẩy Trang Simple Cấp Ẩm Nhẹ Nhàng Làm Sạch Trang Điểm Micellar Cleasing Water 400Ml', 132000, 99, NULL, 'https://product.hstatic.net/200000061028/product/hi_4_e509d477ec5f421f886a51d66b792ffb_grande.jpg', 'Nước tẩy trang cấp ẩm , làm sạch nhẹ nhàng đến từ thương hiệu Simple chứa Vitamin B3, Vitamin C và nước cất 3 lần tinh khiết giúp nhẹ nhàng tẩy sạch lớp trang điểm, kem chống nắng và bụi bẩn trên da hiệu quả. Đồng thời nước tẩy trang Simple Micellar Cleasing Water còn giúp cung cấp thêm dưỡng chất cần thiết cho làn da thêm ẩm mượt, mịn màng và rạng rỡ', 'Nước Tẩy Trang Simple Cấp Ẩm Nhẹ Nhàng Làm Sạch Trang Điểm Micellar Cleasing Water 400Ml\r\n \r\nNước tẩy trang cấp ẩm , làm sạch nhẹ nhàng đến từ thương hiệu Simple chứa Vitamin B3, Vitamin C và nước cất 3 lần tinh khiết giúp nhẹ nhàng tẩy sạch lớp trang điểm, kem chống nắng và bụi bẩn trên da hiệu quả. Đồng thời nước tẩy trang Simple Micellar Cleasing Water còn giúp cung cấp thêm dưỡng chất cần thiết cho làn da thêm ẩm mượt, mịn màng và rạng rỡ\r\n \r\nSản phẩm không chứa: 2000 hóa chất gây hại cho da, hương liệu nhân tạo, phẩm màu nhân tạo, cồn, dầu khoáng hạn chế làm da khô - căng rát hay kích ứng\r\n \r\n\r\n\r\n \r\n\r\nCÔNG DỤNG CHÍNH – MAIN FUNCTION\r\n \r\nGiúp nhẹ nhàng tẩy sạch lớp trang điểm, kem chống nắng và bụi bẩn trên da hiệu quả\r\nCung cấp thêm dưỡng chất cần thiết cho làn da thêm ẩm mượt, mịn màng và rạng rỡ\r\n\r\n\r\n \r\n\r\nTham khảo thêm các sản phẩm tẩy trang khác tại đây. \r\n\r\nCÁCH DÙNG – DIRECTION FOR USE\r\n \r\n- Cho một lượng nước tẩy trang vừa đủ ra bông tẩy trang. Nhẹ nhàng lau sạch lớp trang điểm và bụi bẩn. Không cần chà xát lên da. Không cần rửa lại với nước\r\n- Để đạt được hiệu quả tốt nhất, sau khi tẩy trang  kết hợp cùng sữa rửa mặt và nước hoa hồng\r\n \r\nThành phần: Aqua, Hexylene Glycol, Glycerin, PEG-6 Caprylic/Capric Glycerides,Phenoxyethanol, Cetrimonium Chloride, Tetrasodium EDTA,Propylene Glycol, Citric Acid, Cetylpyridinium Chloride, Sodium Chloride, Niacinamide, Sodium Ascorbyl, Phosphate, Potassium chloride, Panthenol\r\n \r\nĐối tượng sử dụng: Phù hợp cho mọi loại da, kể cả làn da nhạy cảm \r\n \r\nBảo quản: Bảo quản nơi khô ráo, thoáng mát, tránh ánh sáng trực tiếp\r\n \r\nQuy cách: Chai, 400Ml\r\n \r\nXuất xứ thương hiệu: Anh Quốc\r\n \r\nHSD: 3 năm kể từ ngày sản xuất\r\n \r\nXem thêm các sản phẩm khác cùng với nhiều chương trình khuyến mãi hấp dẫn tại website Guardian\r\n\r\n---\r\n\r\nTHÔNG TIN THÊM - ADDITIONAL INFORMATION\r\n\r\n- Để biết thêm thông tin chi tiết về sản phẩm hoặc chương trình khuyến mãi Quý khách hàng vui lòng liên hệ bộ phận CSKH qua Hotline 19004449 hoặc fanpage Guardian Việt Nam để được hỗ trợ\r\n\r\n- Nếu quý khách hàng có nhu cầu xuất hóa đơn, vui lòng điền thông tin xuất hóa đơn vào mục Ghi chú đơn hàng, Bộ phận CSKH sẽ gửi hóa đơn điện tử vào email Quý Khách hàng đã đăng ký với Guardian\r\n\r\nGuardian xin chân thành cảm ơn Quý khách!', '2023-07-17 13:35:15', '2023-07-17 13:35:15', NULL);
+(2, 1, 3, 'Nước Tẩy Trang Simple Cấp Ẩm Nhẹ Nhàng Làm Sạch Trang Điểm Micellar Cleasing Water 400Ml', 132000, 99, NULL, 'https://product.hstatic.net/200000061028/product/hi_4_e509d477ec5f421f886a51d66b792ffb_grande.jpg', 'Nước tẩy trang cấp ẩm , làm sạch nhẹ nhàng đến từ thương hiệu Simple chứa Vitamin B3, Vitamin C và nước cất 3 lần tinh khiết giúp nhẹ nhàng tẩy sạch lớp trang điểm, kem chống nắng và bụi bẩn trên da hiệu quả. Đồng thời nước tẩy trang Simple Micellar Cleasing Water còn giúp cung cấp thêm dưỡng chất cần thiết cho làn da thêm ẩm mượt, mịn màng và rạng rỡ', 'Nước Tẩy Trang Simple Cấp Ẩm Nhẹ Nhàng Làm Sạch Trang Điểm Micellar Cleasing Water 400Ml\r\n \r\nNước tẩy trang cấp ẩm , làm sạch nhẹ nhàng đến từ thương hiệu Simple chứa Vitamin B3, Vitamin C và nước cất 3 lần tinh khiết giúp nhẹ nhàng tẩy sạch lớp trang điểm, kem chống nắng và bụi bẩn trên da hiệu quả. Đồng thời nước tẩy trang Simple Micellar Cleasing Water còn giúp cung cấp thêm dưỡng chất cần thiết cho làn da thêm ẩm mượt, mịn màng và rạng rỡ\r\n \r\nSản phẩm không chứa: 2000 hóa chất gây hại cho da, hương liệu nhân tạo, phẩm màu nhân tạo, cồn, dầu khoáng hạn chế làm da khô - căng rát hay kích ứng\r\n \r\n\r\n\r\n \r\n\r\nCÔNG DỤNG CHÍNH – MAIN FUNCTION\r\n \r\nGiúp nhẹ nhàng tẩy sạch lớp trang điểm, kem chống nắng và bụi bẩn trên da hiệu quả\r\nCung cấp thêm dưỡng chất cần thiết cho làn da thêm ẩm mượt, mịn màng và rạng rỡ\r\n\r\n\r\n \r\n\r\nTham khảo thêm các sản phẩm tẩy trang khác tại đây. \r\n\r\nCÁCH DÙNG – DIRECTION FOR USE\r\n \r\n- Cho một lượng nước tẩy trang vừa đủ ra bông tẩy trang. Nhẹ nhàng lau sạch lớp trang điểm và bụi bẩn. Không cần chà xát lên da. Không cần rửa lại với nước\r\n- Để đạt được hiệu quả tốt nhất, sau khi tẩy trang  kết hợp cùng sữa rửa mặt và nước hoa hồng\r\n \r\nThành phần: Aqua, Hexylene Glycol, Glycerin, PEG-6 Caprylic/Capric Glycerides,Phenoxyethanol, Cetrimonium Chloride, Tetrasodium EDTA,Propylene Glycol, Citric Acid, Cetylpyridinium Chloride, Sodium Chloride, Niacinamide, Sodium Ascorbyl, Phosphate, Potassium chloride, Panthenol\r\n \r\nĐối tượng sử dụng: Phù hợp cho mọi loại da, kể cả làn da nhạy cảm \r\n \r\nBảo quản: Bảo quản nơi khô ráo, thoáng mát, tránh ánh sáng trực tiếp\r\n \r\nQuy cách: Chai, 400Ml\r\n \r\nXuất xứ thương hiệu: Anh Quốc\r\n \r\nHSD: 3 năm kể từ ngày sản xuất\r\n \r\nXem thêm các sản phẩm khác cùng với nhiều chương trình khuyến mãi hấp dẫn tại website Guardian\r\n\r\n---\r\n\r\nTHÔNG TIN THÊM - ADDITIONAL INFORMATION\r\n\r\n- Để biết thêm thông tin chi tiết về sản phẩm hoặc chương trình khuyến mãi Quý khách hàng vui lòng liên hệ bộ phận CSKH qua Hotline 19004449 hoặc fanpage Guardian Việt Nam để được hỗ trợ\r\n\r\n- Nếu quý khách hàng có nhu cầu xuất hóa đơn, vui lòng điền thông tin xuất hóa đơn vào mục Ghi chú đơn hàng, Bộ phận CSKH sẽ gửi hóa đơn điện tử vào email Quý Khách hàng đã đăng ký với Guardian\r\n\r\nGuardian xin chân thành cảm ơn Quý khách!', '2023-07-17 13:35:15', '2023-07-17 13:35:15', NULL),
+(3, 1, 1, 'Nước Tẩy Trang Sạch Thoáng Senka All Clear Water Fresh 230Ml', 83000, 99, NULL, 'https://product.hstatic.net/200000061028/product/fresh_2__1__1f2dd9f61ba9480493bee3e66c1276d8_grande.jpg', 'Nước Tẩy Trang Sạch Thoáng All Clear Water Fresh Senka 230Ml\r\nNước Tẩy Trang Sạch Thoáng All clear water Micellar Fresh với công nghệ Nhật Bản nhẹ nhàng làm sạch lớp trang điểm, bụi bẩn và bã nhờn sâu bên trong lỗ chân lông, loại bỏ dầu thừa và không gây kích ứng trên da\r\n', 'MÔ TẢ SẢN PHẨM – DESCRIPTION\r\nNước Tẩy Trang Sạch Thoáng All Clear Water Fresh Senka 230Ml\r\nNước Tẩy Trang Sạch Thoáng All clear water Micellar Fresh với công nghệ Nhật Bản nhẹ nhàng làm sạch lớp trang điểm, bụi bẩn và bã nhờn sâu bên trong lỗ chân lông, loại bỏ dầu thừa và không gây kích ứng trên da\r\n\r\nCÔNG DỤNG CHÍNH – MAIN FUNCTION\r\n\r\n- Không chỉ đóng vai trò làm sạch lớp makeup, sản phẩm còn đảm bảo duy trì lớp màng dưỡng ẩm tự nhiên cho da. Loại bỏ dầu thừa trên da sau mỗi lần sử dụng\r\n- Với chiết xuất từ lá trà xanh và  tơ tằm trắng, sản phẩm làm sạch sâu lớp cặn trang điểm bám trong lỗ chân lông và loại bỏ dầu thừa đáng ghét nữa nhé\r\n- Sản phẩm cũng không hề để lại lớp bóng dầu như các loại tẩy trang khác. Nên dù các nàng có bận rộn đến mức không thể rửa lại ngay với nước sau khi tẩy trang, làn da của bạn cũng sẽ không nhờn dính và không khô căng\r\n- Đặc biệt, sản phẩm còn có hương thơm dịu nhẹ lưu lại cảm giác tươi mát, dễ chịu ngay sau khi sử dụng\r\n \r\nCÁCH DÙNG – DIRECTION FOR USE \r\n \r\n- Lấy một lượng sản phẩm vừa đủ ra bông tẩy trang. Nhẹ nhàng lau sạch lớp trang điểm\r\n \r\nThành phần: Water (Aqua), Dipropylene Glycol, Peg/Ppg-50/40 Dimethyl Ether, Peg-8, Glycerin, Phenoxyethanol, Methylparaben, Sodium Citrate, Potassium Cocoyl Glutamate, Citric Acid, Butylene Glycol, Sodium Metaphosphate, Sericin, Potassium Sorbate, Camellia Sinensis Leaf Extract, Sodium Acetylated Hyaluronate, Sodium Hyaluronate, Xanthan Gum\r\n \r\nQuy cách: Chai, 230Ml\r\n\r\nBảo quản: Nơi khô ráo, thoáng mát, tránh ánh nắng trực tiếp. Đậy nắp kín sau khi sử dụng\r\n\r\nXuất xứ thương hiệu: Việt Nam\r\n \r\nXuất xứ: Việt Nam \r\n \r\nHSD: 3 năm kể từ ngày sản xuất\r\n \r\nLưu ý: Để xa tầm tay trẻ em. Ngưng sử dụng ngay khi có biểu hiện kích ứng và hỏi ý kiến bác sĩ (nếu cần)\r\n \r\n---\r\n \r\nTHÔNG TIN THÊM - ADDITIONAL INFORMATION\r\n \r\n- Để biết thêm thông tin chi tiết về sản phẩm hoặc chương trình khuyến mãi Quý khách hàng vui lòng liên hệ bộ phận CSKH qua Hotline 19004449 hoặc fanpage https://www.facebook.com/guardianvn để được hỗ trợ\r\n \r\n- Nếu quý khách hàng có nhu cầu xuất hóa đơn, vui lòng điền thông tin xuất hóa đơn vào mục Ghi chú đơn hàng, Bộ phận CSKH sẽ gửi hóa đơn điện tử vào email Quý Khách hàng đã đăng ký với Guardian\r\n \r\nGuardian xin chân thành cảm ơn Quý khách!', '2023-07-18 15:18:29', '2023-07-18 15:18:29', NULL);
 
 -- --------------------------------------------------------
 
@@ -189,9 +184,6 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `fullname`, `email`, `phone_number`, `address`, `password`, `role_id`, `created_at`, `is_manager`) VALUES
 (1, 'Admin', 'admin@admin.com', '123456789', NULL, '21232f297a57a5a743894a0e4a801fc3', 1, NULL, NULL),
-(2, 'asdasd', 'admin123', '2324634343', NULL, '93279e3308bdbbeed946fc965017f67a', NULL, NULL, NULL),
-(3, 'asdasd', 'admin123', '2324634343', NULL, '93279e3308bdbbeed946fc965017f67a', NULL, NULL, NULL),
-(4, 'asdasd', 'admin123', '2324634343', NULL, '93279e3308bdbbeed946fc965017f67a', NULL, NULL, NULL),
 (5, 'Giang', 'giang123@gmail.com', '0399388692', NULL, 'f5bb0c8de146c67b44babbf4e6584cc0', NULL, NULL, NULL);
 
 --
@@ -236,8 +228,8 @@ ALTER TABLE `order_details`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `brand_id` (`brand_id`),
-  ADD KEY `category_id` (`category_id`);
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `brand_id` (`brand_id`) USING BTREE;
 
 --
 -- Chỉ mục cho bảng `roles`
@@ -278,19 +270,19 @@ ALTER TABLE `feedback`
 -- AUTO_INCREMENT cho bảng `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT cho bảng `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT cho bảng `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT cho bảng `roles`
